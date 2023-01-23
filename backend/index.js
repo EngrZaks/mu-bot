f1ce373c699fb704d7718617db2243f7b296caee
EngrZaks <abzakariyya@gmail.com>
2023-01-23 22:01:02 +0100

    changed credentials to use env

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const fs = require("fs");
 const express = require("express");
 var bodyParser = require("body-parser");
-const { query } = require("express");
+const uuid = require("uuid");
 
 // initialise express app
 const app = express();
@@ -19,10 +19,12 @@ app.use(
 );
 
 // gcloud credentials
-const CREDENTIALS = JSON.parse(fs.readFileSync("credentials.json"));
+// const CREDENTIALS = JSON.parse(fs.readFileSync("credentials.json"));
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 
 // gcloud project ID
 const PROJECTID = CREDENTIALS.project_id;
+console.log(PROJECTID);
 
 // dialogflow client configuration
 const CONFIGURATION = {
@@ -68,7 +70,6 @@ app.get("/", (req, res) => res.send("hello from MU Chatbot"));
 // intent detection route
 app.post("/detect", async (req, res) => {
   const { queryText, sessionId } = req.body;
-  console.log(queryText, sessionId, req.body);
   try {
     const data = await detectIntent("en", queryText, sessionId);
     res.status(200).send(data);
