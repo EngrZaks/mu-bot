5ccfa25114977c0145d251c777ff246b2d16c9b6
EngrZaks <abzakariyya@gmail.com>
2023-03-17 07:54:51 +0100

    refactor: moved scrapper to a separate module

--- frontend/api/fulfillment.js
+++ frontend/api/fulfillment.js
@@ -1,6 +1,5 @@
-import * as cheerio from "cheerio";
-import axios from "axios";
 import client from "dialogflow-fulfillment";
+import { scrapeNews } from "./_scrapper";
 
 export default function getNews(req, res) {
   const agent = new client.WebhookClient({ request: req, response: res });
@@ -24,23 +23,3 @@ export default function getNews(req, res) {
 }
 
 // scrapper function
-async function scrapeNews() {
-  let news = [];
-  const response = await axios.request({
-    method: "GET",
-    url: "https://www.mdx.ac.uk/news",
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-    },
-  });
-  const $ = cheerio.load(response.data);
-  $(".listing-news-item > ul")
-    .find("li")
-    .each((index, element) => {
-      const headline = $(element).find("h2").text();
-      const summary = $(element).find("a > p").text();
-      news.push({ headline, summary });
-    });
-  return news;
-}
