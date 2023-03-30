d98112d5e4b5dd98da635c3bf6e8357e41ce0ae0
EngrZaks <abzakariyya@gmail.com>
2023-03-30 12:00:40 +0100

    added the ability to get available masters r undergrad scholarships dynaically

--- frontend/api/fulfillment.ts
+++ frontend/api/fulfillment.ts
@@ -34,10 +34,97 @@ export default function fulfillmentHandler(
     response += "</div>";
     agent.add(response);
   }
+  async function getScholarships(agent: any) {
+    const scholarshipType = agent.parameters["scholarship-type"];
+
+    let response = "";
+    switch (scholarshipType) {
+      case "undergraduate":
+      case "undergraduates":
+      case "degree":
+      case "bachelor":
+        response = `
+      <h3>The following are the available scholarships for undergraduates</h3>
+      <div style="padding:5px; background:#e3e3e3; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">MDX starter kit </p>
+        <a href="https://www.mdx.ac.uk/study-with-us/fees-and-funding/scholarships-and-bursaries/financial-packages-support/" target="_blank"> details</a>
+      </div>
+      <div style="padding:5px; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">The MDX Excellence Scholarship </p>
+        <a href="https://www.mdx.ac.uk/study-with-us/fees-and-funding/scholarships-and-bursaries/financial-packages-support" target="_blank"> details</a>
+      </div>
+      <div style="padding:5px; background:#e3e3e3; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">The Santander Universities Scholars Programme </p>
+        <a href="https://app.becas-santander.com/en/program/santander-scholarships-skills-santander-universities-scholars-programme" target="_blank"> details</a>
+      </div>
+      <div style="padding:5px;  display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">The Southern Housing Study4Success Award </p>
+      </div>
+      <div style="padding:5px; background:#e3e3e3; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">Whatuni Financial Support Bursaries </p>
+        <a href="mailto:MDXBursaries@mdx.ac.uk">email ✉️</a>
+      </div>
+      <div style="padding:5px;  display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">European Academic Awards </p>
+        <a href="mailto:EU-enquiries@mdx.ac.uk" target="_blank">email ✉️</a>
+      </div>
+      <div style="padding:5px; background:#e3e3e3; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">Significant achievement in sport </p>
+        <a href="mailto:scholarships@mdx.ac.uk" target="_blank"> email ✉️</a>
+      </div>
+      <p>
+      Visit <a href="https://www.mdx.ac.uk/study-with-us/fees-and-funding/scholarships-and-bursaries">our scholarship website</a> for more info on eligibility, how to apply and about other scholarships such as Course Scholarships, Scholarships for Women in STEM, etc.
+      </p>
+      `;
+        break;
+      case "postgraduate":
+      case "postgraduates":
+      case "graduate":
+      case "graduates":
+      case "alumni":
+      case "masters":
+      case "phd":
+      case "doctorate":
+        response = `
+      <h3>The following are the available scholarships for postgraduates</h3>
+      <div style="padding:5px; background:#e3e3e3; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">The Alumni Bursary  </p>
+        <a href="https://wgfp-prrw02.mdx.ac.uk:8001/ticket/portalticketcreation.aspx" target="_blank"> check eligibility</a>
+      </div>
+      <div style="padding:5px;  display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">Commonwealth scholarship  </p>
+        <a href="https://cscuk.dfid.gov.uk/apply" target="_blank"> details</a>
+      </div>
+      <div style="padding:5px; background:#e3e3e3; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">Chevening scholarship  </p>
+        <a href="https://www.chevening.org/apply" target="_blank"> details</a>
+      </div>
+      <div style="padding:5px;  display:flex; justify-content:space-between;"> 
+        <p style="margin:0;">European Academic Awards  </p>
+        <a href="" target="_blank"> details</a>
+      </div>
+      <div style="padding:5px; background:#e3e3e3; display:flex; justify-content:space-between;"> 
+        <p style="margin:0;"> </p>
+       <span> <a href="mailto:EU-enquiries@mdx.ac.uk" target="_blank"> email ✉️</a> | 
+        <a href="https://app.geckoform.com/public/#/modern/21FO00g4rozmi800bwt5wrejxb" target="_blank"> apply</a></span>
+      </div>
+      <p>
+      Visit <a href="https://www.mdx.ac.uk/study-with-us/fees-and-funding/scholarships-and-bursaries">our scholarship website</a> for more info on eligibility, how to apply and about other scholarships such as Course Scholarships, Scholarships for Women in STEM, etc.
+      </p>
+      `;
+        break;
+      default:
+        response = `Visit <a href="https://www.mdx.ac.uk/study-with-us/fees-and-funding/scholarships-and-bursaries">our scholarship website</a> for more info about scholarships.`;
+        break;
+    }
+
+    agent.add(response);
+  }
 
   let intents = new Map();
   intents.set("GetNews", sendNews);
   intents.set("Events", getEvents);
+  intents.set("scholarships", getScholarships);
   agent.handleRequest(intents);
 }
 
